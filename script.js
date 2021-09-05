20b75d254b6917b48a5cd2f017ff10e64ca1ca75
Jeff <lagosjf@gmail.com>
2021-09-05 22:00:16 +0200

    prototype

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function Book (title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.info = function() {
+        return this.title + " by " + this.author + " contain " + this.pages;
+    }
+}
+const hobit = new Book ("Hobit", "Moi meme", "640")
+
+Book.prototype.newfonc = function () {
+    return this.title + "hols"
+}
+
+document.getElementById("book").innerHTML = hobit.newfonc();
+console.log(hobit.info())
